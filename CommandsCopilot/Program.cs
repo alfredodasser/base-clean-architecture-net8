1241f421c5bb2b3eb5a7d7c6e18ffc3674f79d0c
Alfredo Benaute <alfredo.benaute@gmail.com>
2025-10-14 18:56:15 -0500

    fix: add console project for commands copilot

---
+++ CommandsCopilot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Calculadora
+{
+    public int Sumar(int a, int b) => a + b;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var calc = new Calculadora();
+        Console.WriteLine(calc.Sumar(2, 3));
+    }
+}

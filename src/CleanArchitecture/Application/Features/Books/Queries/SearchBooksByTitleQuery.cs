55a94cc40b7109c125ec9b02de846780b77c5340
Alfredo Benaute <alfredo.benaute@gmail.com>
2025-10-17 21:35:31 -0500

    fix: query search books

--- src/CleanArchitecture/Application/Features/Books/Queries/SearchBooksByTitleQuery.cs
+++
@@ -1,7 +0,0 @@
-using CleanArchitecture.Shared.Models;
-using CleanArchitecture.Shared.Models.Book;
-using MediatR;
-
-namespace CleanArchitecture.Application.Features.Books.Queries;
-
-public record SearchBooksByTitleQuery(string Title, int PageIndex = 0, int PageSize = 10) : IRequest<Pagination<BookDTO>>;
--- src/CleanArchitecture/Application/Features/Books/Queries/SearchBooksByTitleQueryHandler.cs
+++ src/CleanArchitecture/Application/Features/Books/Queries/SearchBooksByTitleQueryHandler.cs
@@ -5,6 +5,7 @@ using MediatR;
 
 namespace CleanArchitecture.Application.Features.Books.Queries;
 
+public record SearchBooksByTitleQuery(string Title, int PageIndex = 0, int PageSize = 10) : IRequest<Pagination<BookDTO>>;
 public class SearchBooksByTitleQueryHandler : IRequestHandler<SearchBooksByTitleQuery, Pagination<BookDTO>>
 {
     private readonly IBookService _bookService;
